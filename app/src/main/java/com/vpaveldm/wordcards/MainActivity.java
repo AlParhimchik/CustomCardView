ad304ec4518cd714e87c15a47f7ce110cc787155
Pavel Vaitsikhousky <vpaveldm@gmail.com>
2017-09-25 16:10:23 +0300

    new commit

--- app/src/main/java/com/vpaveldm/wordcards/MainActivity.java
+++ app/src/main/java/com/vpaveldm/wordcards/MainActivity.java
@@ -1,21 +1,18 @@
 package com.vpaveldm.wordcards;
 
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     private FragmentManager mManager;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mManager = getSupportFragmentManager();
         Fragment fragment = mManager.findFragmentById(R.id.container);
         if (fragment == null){
