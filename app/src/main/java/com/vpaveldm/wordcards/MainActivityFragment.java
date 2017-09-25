1cec82542e80014c0049bbc1b5ca6272f21d4383
Pavel Vaitsikhousky <vpaveldm@gmail.com>
2017-09-25 15:45:16 +0300

    some commit13

--- app/src/main/java/com/vpaveldm/wordcards/MainActivityFragment.java
+++ app/src/main/java/com/vpaveldm/wordcards/MainActivityFragment.java
@@ -1,21 +1,10 @@
 package com.vpaveldm.wordcards;
 
-import android.graphics.Point;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.CardView;
-import android.util.Log;
-import android.view.Display;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivityFragment extends Fragment {
 
